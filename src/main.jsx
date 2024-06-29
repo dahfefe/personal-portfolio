9a5396a1904ea975e6ecdf300848f77caf2680f7
Felix Teng <fsmunozteng@gmail.com>
2024-06-29 05:04:45 -0700

    added content info js file for personal details

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
